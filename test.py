670975eedca138e074dc93cd2e886628b47ae8b7
jennifermei <jennifermei95@gmail.com>
2023-11-15 23:57:39 -0800

    created test.py

--- test.py
+++ test.py
@@ -13,9 +13,11 @@ state_abbrs = abbrs()
 state_names = list(state_abbrs)
 state_abbrs = list(state_abbrs.values())
 
+years = [2014, 2016]
+state_names = ["Alaska", "New Hampshire"]
+
 for year in years: 
     for state in state_names:
-        print(state)
 
         url = get_url(state, year)
         
@@ -24,41 +26,91 @@ for year in years:
 
         soup = BeautifulSoup(html_content, 'html.parser')
 
+        table = soup.find('table', {'class': 'wikitable plainrowheaders sortable'})
+
         districts = []
         democratic_votes = []
         republican_votes = []
         total_votes = []
 
-        table_check = soup.find("table", {"class": "wikitable plainrowheaders"})
-        tables = soup.find_all("table", {"class": "wikitable plainrowheaders"})
+        if table:
+            for row in table.find_all('tr')[1:]:
+                columns = row.find_all('td')
+                if columns:
+                    district_name = columns[0].text
+                    district_name = district_name[len("District "):]
 
-        if table_check:
+                    top_row = table.find_all('tr')[0].text
+                    party = top_row.split()[1]
+                    if party == "Republican":
+                        rep_votes = replace_if_not_number(columns[1].text)
+                        rep_votes = int(rep_votes.replace(',', ''))
+                        dem_votes = replace_if_not_number(columns[3].text)
+                        dem_votes = int(dem_votes.replace(',', ''))
+                    if party == "Democratic":
+                        rep_votes = replace_if_not_number(columns[3].text)
+                        rep_votes = int(rep_votes.replace(',', ''))
+                        dem_votes = replace_if_not_number(columns[1].text)
+                        dem_votes = int(dem_votes.replace(',', ''))   
+
+                    if columns[-2].text == "100%" or columns[-2].text == "100.0%" or columns[-2].text == "100.00%":
+                        tot_votes = replace_if_not_number(columns[-3].text)
+                    else:
+                        tot_votes = tot_votes = replace_if_not_number(columns[-2].text)
+                    tot_votes = int(tot_votes.replace(',', ''))
+
+                    districts.append(district_name)
+                    republican_votes.append(rep_votes)
+                    democratic_votes.append(dem_votes)
+                    total_votes.append(tot_votes)
+
+            if len(districts) > 1:
+                districts.pop()
+                republican_votes.pop()
+                democratic_votes.pop()
+                total_votes.pop()
+
+        elif "elections" in url:
+            tables = soup.find_all("table", {"class": "wikitable plainrowheaders"})
+
+            district_name = 1
+
             for table_cd in tables:
                 caption = table_cd.find("caption")
-                if caption and "congressional district election" in caption.get_text().lower():
+
+                if caption and "congressional district" in caption.get_text().lower():
                     table = table_cd
                 else:
-                    table = None
-                    # break
+                    continue
 
-                # if table:
-                print(table)
-                for row in table.find_all("tr"):
-                    cells = row.find_all(["th", "td"])
-                    district = caption.get_text()
-                    if len(cells) >= 3:
-                        party = cells[1].text.strip()
+                if table:
+                    dem_votes = 0
+                    rep_votes = 0
 
-                        if "Republican" in party:
-                            rep_votes = replace_if_not_number(cells[3].text.strip())
-                            rep_votes = int(rep_votes.replace(",", ""))
-                        elif "Democratic" in party:
-                            dem_votes = replace_if_not_number(cells[3].text.strip())
-                            dem_votes = int(dem_votes.replace(",", ""))
-                        elif any(char.isdigit() for char in party):
-                            tot_votes = party
-                            tot_votes = int(tot_votes.replace(",", ""))
+                    for row in table.find_all("tr"):
+                        cells = row.find_all(["th", "td"])
+                        if len(cells) >= 3:
+                            party = cells[1].text.strip()
 
+                            if "Republican" in party:
+                                rep_votes_append = replace_if_not_number(cells[3].text.strip())
+                                rep_votes_append = int(rep_votes_append.replace(",", ""))
+                                rep_votes += rep_votes_append
+                            elif "Democratic" in party:
+                                dem_votes_append = replace_if_not_number(cells[3].text.strip())
+                                dem_votes_append = int(dem_votes_append.replace(",", ""))
+                                dem_votes += dem_votes_append
+                            elif any(char.isdigit() for char in party):
+                                tot_votes = party
+                                tot_votes = int(tot_votes.replace("'", "").replace(",", ""))
+
+                    districts.append(district_name)
+                    republican_votes.append(rep_votes)
+                    democratic_votes.append(dem_votes)
+                    total_votes.append(tot_votes)
+                    district_name += 1
+
+        # for if it is a single district state
         else:
             table_single = None
             tables_single = soup.find_all("table", {"class": "wikitable plainrowheaders"})
@@ -71,27 +123,35 @@ for year in years:
 
             if table_single:
                 district_name = 1
+                dem_votes = 0
+                rep_votes = 0
+
                 for row in table_single.find_all("tr"):
                     cells = row.find_all(["th", "td"])
                     if len(cells) >= 3:
                         party = cells[1].text.strip()
 
                         if "Republican" in party:
-                            rep_votes = replace_if_not_number(cells[3].text.strip())
-                            rep_votes = int(rep_votes.replace(",", ""))
+                            rep_votes_append = replace_if_not_number(cells[3].text.strip())
+                            rep_votes_append = int(rep_votes_append.replace(",", ""))
+                            rep_votes += rep_votes_append
                         elif "Democratic" in party:
-                            dem_votes = replace_if_not_number(cells[3].text.strip())
-                            dem_votes = int(dem_votes.replace(",", ""))
+                            dem_votes_append = replace_if_not_number(cells[3].text.strip())
+                            dem_votes_append = int(dem_votes_append.replace(",", ""))
+                            dem_votes += dem_votes_append
                         elif any(char.isdigit() for char in party):
                             tot_votes = party
-                            tot_votes = int(tot_votes.replace(",", ""))
+                            tot_votes = int(tot_votes.replace("'", "").replace(",", ""))
 
                 districts.append(district_name)
                 republican_votes.append(rep_votes)
                 democratic_votes.append(dem_votes)
                 total_votes.append(tot_votes)
 
-        # Create a DataFrame
+        if year == 2016 and state == "Illinois":
+            republican_votes[9] = "135535"
+            total_votes[9] = "285996"
+
         data = {
             'District': districts,
             'Democratic Votes': democratic_votes,
@@ -101,11 +161,8 @@ for year in years:
             'State': state
         }
 
-        print(data)
-
         df = pd.DataFrame(data)
 
-        # Concatenate the current DataFrame to the all_data DataFrame
         all_data = pd.concat([all_data, df], ignore_index=True)
 
 all_data['Democratic Votes'] = all_data['Democratic Votes'].astype(int)
@@ -113,4 +170,4 @@ all_data['Republican Votes'] = all_data['Republican Votes'].astype(int)
 all_data['Total Votes'] = all_data['Total Votes'].astype(int)
 all_data['Year'] = all_data['Year'].astype(int)
 
-all_data.to_csv("wiki_elections.csv", index=False)
+all_data.to_csv("test.csv", index=False)
