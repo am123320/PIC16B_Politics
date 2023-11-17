0b6653de6156d76aa0019028984f40f0c698f8b2
jennifermei <jennifermei95@gmail.com>
2023-11-17 12:28:44 -0800

    update full data

--- full_data.py
+++ full_data.py
@@ -18,10 +18,28 @@ df_final['Republican Percentage'] = (df_final['Republican Votes'] / df_final['To
 df_final['Democratic Percentage'] = df_final['Democratic Percentage'].round(2)
 df_final['Republican Percentage'] = df_final['Republican Percentage'].round(2)
 
+df_final['Voting Eligible Male'] = df_final["Male 18+ birthright"] + df_final["Male 18+ bloodright"] + df_final["Male 18+ naturalized"]
+df_final['Voting Eligible Female'] = df_final["Female 18+ birthright"] + df_final["Female 18+ bloodright"] + df_final["Female 18+ naturalized"]
+df_final['Voting Eligible Population'] = df_final['Voting Eligible Male'] + df_final['Voting Eligible Female']
+df_final['Voter Turnout'] = (df_final['Total Votes'] / df_final['Voting Eligible Population']) * 100
+df_final['Voter Turnout'] = df_final['Voter Turnout'].round(2)
+
 first_columns = ['Year', 'State', 'State Abbr', 'District', 'Democratic Votes', 'Republican Votes', 'Total Votes',
-                  'Democratic Percentage', 'Republican Percentage']
+                  'Democratic Percentage', 'Republican Percentage', 'Voting Eligible Male', 'Voting Eligible Female', 
+                  'Voting Eligible Population', 'Total Population', 'Voter Turnout']
 df_data = df_final[first_columns + [col for col in df_final.columns if col not in first_columns]]
 
-#remove_penn = (df_data['Year'] == 2016) & (df_data['State'] == 'Pennsylvania') & (df_data['District'] == '19')
-#df_data = df_data[~remove_penn]
+keep_as_float = ['Democratic Percentage', 'Republican Percentage', 
+                 'Voter Turnout', 'Year', 'State', 'State Abbr', 
+                 'District', 'Median Age']
+change_to_int = [col for col in df_data.columns if col not in keep_as_float]
+df_data[change_to_int] = df_data[change_to_int].fillna(0).astype(int)
+df_data[change_to_int] = df_data[change_to_int].astype(int)
+
+
+remove_pa19 = (df_data['Year'] == 2016) & (df_data['State'] == 'Pennsylvania') & (df_data['District'] == '19')
+remove_ca44 = (df_data['Year'] == 2020) & (df_data['State'] == 'California') & (df_data['District'] == '44')
+df_data = df_data[~remove_pa19]
+df_data = df_data[~remove_ca44]
+
 df_data.to_csv('final.csv', index=False)
